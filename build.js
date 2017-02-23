1e27b8b509630113f7fec219fbc77bf886361547
Valeriya Edemskaya <Valeriya@Valeriyas-Air.hitronhub.home>
2017-02-22 19:44:06 -0800

    made simple build file

--- build.js
+++ build.js
@@ -4,4 +4,11 @@ var PARTIALS_DIR = "./src/templates/partials";
 var MAIN_TEMPLATE = "./src/templates/index.hbs";
 var OUTPUT_FILE = "./output.html";
 
+var fs = require("fs");
+var Handlebars = require("handlebars");
+var json = require("./src/example.json")
 
+var template = fs.readFileSync(MAIN_TEMPLATE, "utf8");
+var render = Handlebars.compile(template);
+var html = render(json);
+console.log(html);
