63ca5077cdef66c9ff7ba7797f79c1068e31d20a
Valeriya Edemskaya <Valeriya@Valeriyas-Air.hitronhub.home>
2017-02-27 15:03:17 -0800

    started handlebars file

--- build.js
+++ build.js
@@ -1,14 +1,20 @@
 #! /usr/bin/env node
 
-var PARTIALS_DIR = "./src/templates/partials";
+// var PARTIALS_DIR = "./src/templates/partials";
 var MAIN_TEMPLATE = "./src/templates/index.hbs";
-var OUTPUT_FILE = "./output.html";
+var OUTPUT_FILE = "./build/index.html";
+var DATA_FILE = "./src/data.json";
+
+Object.prototype.toString = function () {
+  return JSON.stringify(this);
+};
 
 var fs = require("fs");
 var Handlebars = require("handlebars");
-var json = require("./src/example.json")
+var data = require(DATA_FILE);
 
 var template = fs.readFileSync(MAIN_TEMPLATE, "utf8");
-var render = Handlebars.compile(template);
-var html = render(json);
-console.log(html);
+var toHTML = Handlebars.compile(template);
+var html = toHTML(data);
+
+fs.writeFileSync(OUTPUT_FILE, html, "utf8");
