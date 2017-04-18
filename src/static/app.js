8ca153d71c28b2f0cdc35187b0ca4b97e8eb77c4
Dhruv Dang <hi@dhruv.io>
2017-04-17 18:26:51 -0700

    use jquery instead of css for interaction states

---
+++ src/static/app.js
@@ -0,0 +1,28 @@
+(function () {
+
+  // define a constant so it can be configured easily in the future
+  var ICE_CREAM_SELECTOR = "#gallery article";
+  var HOVER_CLASS = "hover";
+  var ACTIVE_CLASS = "active";
+
+  var iceCreams = $(ICE_CREAM_SELECTOR);
+
+  // handle the hover state
+  iceCreams.on("mouseenter", function () {
+    $(this).toggleClass(HOVER_CLASS, true);
+  });
+  iceCreams.on("mouseleave", function () {
+    $(this).toggleClass(HOVER_CLASS, false);
+    $(this).toggleClass(ACTIVE_CLASS, false);
+  });
+
+  // handle the active state
+  iceCreams.on("mousedown", function () {
+    $(this).toggleClass(ACTIVE_CLASS, true);
+  });
+  iceCreams.on("mouseup", function () {
+    $(this).toggleClass(ACTIVE_CLASS, false);
+  });
+
+
+})() // IIFE: Immediately-Invoked Function Expression
