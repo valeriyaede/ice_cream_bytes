c10ce2e11e04df5ca9e0d8ed1e9e96bd1df9b0ff
Valeriya Edemskaya <Valeriya@Valeriyas-Air.hitronhub.home>
2017-04-19 17:16:59 -0700

    added touchstart and touchend for mobile

--- src/static/app.js
+++ src/static/app.js
@@ -24,5 +24,13 @@
     $(this).toggleClass(ACTIVE_CLASS, false);
   });
 
+    // handle the touch state
+    iceCreams.on("touchstart", function () {
+      $(this).toggleClass(ACTIVE_CLASS, true);
+    });
+    iceCreams.on("touchend", function () {
+      $(this).toggleClass(ACTIVE_CLASS, false);
+    });
+
 
 })() // IIFE: Immediately-Invoked Function Expression
