fe1543adb6981fd31a6f904a3a009e80437c1e41
Dhruv Dang <hi@dhruv.io>
2017-04-20 12:56:56 -0700

    add modernizr and bind js events at the right time

--- src/static/app.js
+++ src/static/app.js
@@ -7,30 +7,31 @@
 
   var iceCreams = $(ICE_CREAM_SELECTOR);
 
-  // handle the hover state
-  iceCreams.on("touchstart", function () {
-    $(this).toggleClass(HOVER_CLASS, true);
-  });
-  iceCreams.on("touchend", function () {
-    $(this).toggleClass(HOVER_CLASS, false);
-    $(this).toggleClass(ACTIVE_CLASS, false);
-  });
-
-  // handle the active state
-  iceCreams.on("mousedown", function () {
-    $(this).toggleClass(ACTIVE_CLASS, true);
-  });
-  iceCreams.on("mouseup", function () {
-    $(this).toggleClass(ACTIVE_CLASS, false);
-  });
-
-    // handle the touch state
+  if (Modernizr.touch == true) {
+    // handle events for mobile devices
     iceCreams.on("touchstart", function () {
+      $(this).toggleClass(HOVER_CLASS, true);
       $(this).toggleClass(ACTIVE_CLASS, true);
     });
     iceCreams.on("touchend", function () {
+      $(this).toggleClass(HOVER_CLASS, false);
       $(this).toggleClass(ACTIVE_CLASS, false);
     });
-
+  } else {
+    // handle events for non-touch devices (presumably with a mouse)
+    iceCreams.on("mouseenter", function () {
+      $(this).toggleClass(HOVER_CLASS, true);
+    });
+    iceCreams.on("mouseleave", function () {
+      $(this).toggleClass(HOVER_CLASS, false);
+      $(this).toggleClass(ACTIVE_CLASS, false);
+    });
+    iceCreams.on("mousedown", function () {
+      $(this).toggleClass(ACTIVE_CLASS, true);
+    });
+    iceCreams.on("mouseup", function () {
+      $(this).toggleClass(ACTIVE_CLASS, false);
+    });
+  }
 
 })() // IIFE: Immediately-Invoked Function Expression
